1ba1ac196a6112d93a7d5cdba00358df1ec56f1f
Wiktor Phillips <wsphillips@gmail.com>
2020-08-13 16:03:50 +0200

    .

--- src/PVCAM.jl
+++ src/PVCAM.jl
@@ -14,6 +14,8 @@ const CAMERA_HANDLE = Ref{Int16}(-1)
 const FRAME_OFFSET = Ref{Int64}(0)
 ROIS = [PL.rgn_type(0,1199,1,0,1199,1)]
 const EXPOSURE_MODE = Ref{Int16}(PL.TIMED_MODE)
+const CIRC_BUFFER = Ref{Vector{UInt16}}()
+const CB_START = Ref{Ptr{UInt16}}(0)
 
 atexit() do
     PL.pvcam_uninit()
@@ -54,15 +56,15 @@ function setup_cont(camera_handle = CAMERA_HANDLE[], region_array = ROIS, exp_mo
     return Int(exp_bytes[])
 end
 
-function start_cont(camera_handle, circ_buffer_mem, circ_buffer_size = UInt32(length(circ_buffer_mem) * sizeof(UInt16)))
+function start_cont(camera_handle, circ_buffer_size = UInt32(length(CIRC_BUFFER[]) * sizeof(UInt16)))
 
-    Bool(PL.exp_start_cont(camera_handle, circ_buffer_mem, UInt32(circ_buffer_size))) || @warn "Unable to start continuous acquisition."
+    Bool(PL.exp_start_cont(camera_handle, CIRC_BUFFER[], UInt32(circ_buffer_size))) || @warn "Unable to start continuous acquisition."
 
     return println("Continuous acquisition started...")
 end
 
 function get_latest_frame_ptr(camera_h)
-    frame_address = Ref{Ptr{UInt16}}()
+    frame_address = Ref{Ptr{UInt16}}(0)
     Bool(PL.exp_get_latest_frame(camera_h, frame_address)) || @warn "Unable to poll for latest frame."
     return frame_address[]
 end
@@ -76,22 +78,21 @@ function stop_cont(camera_h = CAMERA_HANDLE[])
     end
 end
 
-function latest_frame(buffer, camera_h = CAMERA_HANDLE[])
+function latest_frame(camera_h = CAMERA_HANDLE[], start_ptr = CB_START[])
     current_ptr = get_latest_frame_ptr(camera_h)
-    start_ptr = pointer(buffer)
     start_idx = Int((current_ptr - start_ptr) / sizeof(UInt16)) + 1
     stop_idx = start_idx + FRAME_OFFSET[]
-    return start_idx:stop_idx
+    return view(CIRC_BUFFER[], start_idx:stop_idx)
 end
 
 function polled_cont!(; camera_handle = CAMERA_HANDLE[], regions = ROIS, exposure_mode = EXPOSURE_MODE[], exposure_time = 40)
-    
     exposure_bytes = setup_cont(camera_handle, regions, exposure_mode, exposure_time)
     circ_buffer_frames = 20
-    circ_buffer = Vector{UInt16}(undef, Int((circ_buffer_frames * exposure_bytes) / sizeof(UInt16)))
+    CIRC_BUFFER[] = Vector{UInt16}(undef, Int((circ_buffer_frames * exposure_bytes) / sizeof(UInt16)))
     FRAME_OFFSET[] = Int(exposure_bytes / sizeof(UInt16)) - 1
-    start_cont(camera_handle, circ_buffer)
-    return circ_buffer
+    CB_START[] = pointer(CIRC_BUFFER[])
+    start_cont(camera_handle)
+    return
 end
 
 end # module
